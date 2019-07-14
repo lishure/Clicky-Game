f959292bc86bb529ba8e12f7c0a5c68a2b4895c3
Lishure <HUEY_LISA@YAHOO.COM>
2019-07-13 20:38:16 -0700

    Updates

---
+++ precache-manifest.b9775a0cdd033fe92fbd7917afdb1ae8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d25d97267076c3ed653698fb2dbdebb9",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "909670749fe3e4938b56",
+    "url": "/Clicky-Game/static/css/main.7c84a6e5.chunk.css"
+  },
+  {
+    "revision": "49fe2bb6337391b45054",
+    "url": "/Clicky-Game/static/js/2.6e39e941.chunk.js"
+  },
+  {
+    "revision": "909670749fe3e4938b56",
+    "url": "/Clicky-Game/static/js/main.9a1c4a3b.chunk.js"
+  },
+  {
+    "revision": "fab82da4d9420c34bc57",
+    "url": "/Clicky-Game/static/js/runtime~main.012bace8.js"
+  }
+]);
